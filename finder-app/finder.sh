3a23a1d0be3e4f9ffb5277b2369f9dbcc7de74f9
Craig Bost <craigb29@live.com>
2025-10-19 19:49:16 -0400

    added finder.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+#chatgpt utilized for syntax and invididual command formulation (not whole implementation)
+
+echo "You passed $# arguments."
+
+arg_count=$#
+
+
+if [ ${arg_count} != 2 ]; then
+echo "fail, must have 2 arguments"
+exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ -d $filesdir ];then
+    echo "directory exists!"
+else
+    echo "directory DOES NOT EXIST"
+    exit 1
+fi
+
+
+# get file count in folder
+file_count=$(find ${filesdir} -type f | wc -l)
+
+
+# get the number of string matches
+match_line_count=$(find ${filesdir} | grep -r ${searchstr} | wc -l)
+
+#print
+echo "The number of files are ${file_count} and the number of matching lines are ${match_line_count}"
+
+#if[0 != cd ${filesdir}];then
+#exit 1
+#fi
+
+#filesdir echo "Hello, $1!"
+#searchstr echo "Your favorite color is $2."
+
+
+#exit 1
