7512f915daa8a09872ffce93f8eb9d4cb5dde733
Craig Bost <craigb29@live.com>
2025-10-20 17:42:08 -0400

    clean up file

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# check argument count is correct
 arg_count=$#
 
 if [ ${arg_count} != 2 ]; then
@@ -7,23 +8,21 @@ if [ ${arg_count} != 2 ]; then
 exit 1
 fi
 
-writefile=$1 #full path to file (including name)
+#acquire arguments (full path to file including name AND write string)
+writefile=$1 
 writestr=$2
 
 
+#make the folder and file if they do not exist
 if mkdir -p "$(dirname "$writefile")" && touch "$writefile"; then
  : #echo sucesffuly made directory and file
 else
- : #echo failed to make directory anf file
+ : #echo failed to make directory and file
 fi
 
-#if touch "$writefile"; then
-#echo "successfully created file, ${writefile}"
-#else
-#echo "error, invalid 'writefile' argument"
-#exit 1
-#fi
 
+#write string to file
 echo "$writestr" > "$writefile"
 
+#success
 exit 0
